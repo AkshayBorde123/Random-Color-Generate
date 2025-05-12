017f0dffa9a38fff81b18fb42b8c898141fada60
AkshayBorde123 <bordeakshay2004@gmail.com>
2025-05-12 15:29:24 +0530

    update link js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+function generateColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
 
-
+  let color = `${red} , ${green} ,${blue}`;
+}
