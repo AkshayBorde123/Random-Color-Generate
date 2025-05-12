5d591912bd9cd1e8edc0bba3f61381a47ea02349
AkshayBorde123 <bordeakshay2004@gmail.com>
2025-05-12 15:40:53 +0530

    random color is work properly in js

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  let h1 = document.querySelector("h1");
+  h1.innerText = generateColor();
+  let box = document.querySelector("#box");
+  box.style.backgroundColor = generateColor();
+});
+
 function generateColor() {
   let red = Math.floor(Math.random() * 255);
   let green = Math.floor(Math.random() * 255);
   let blue = Math.floor(Math.random() * 255);
 
-  let color = `${red} , ${green} ,${blue}`;
+  let color = `rgb(${red},${green},${blue})`;
+  return color;
 }
